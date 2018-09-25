76f11c249116ca779c7ed439163ec3e29a72d2eb
Meir Cohen <meirkoen@gmail.com>
2018-09-25 17:55:49 +0300

    parsing payloads with StringDecoder

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+
+const decoder = new StringDecoder('utf-8')
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true)
   const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+  let payload = ''
 
-  res.end(`
-    Request recived on path: ${trimmedPath}
-    with method: ${req.method.toUpperCase()}
-    and with these query string parameters: ${JSON.stringify(parsedUrl.query)}
-    and with these headers: ${JSON.stringify(req.headers)}
-    `)
+  req.on('data', data => payload += decoder.write(data))
+  req.on('end', () => {
+    payload += decoder.end()
+
+    res.end(`
+      Request recived on path: ${trimmedPath}
+      with method: ${req.method.toUpperCase()}
+      and with these query string parameters: ${JSON.stringify(parsedUrl.query)}
+      and with these headers: ${JSON.stringify(req.headers)}
+      and with this payload; ${payload}
+      `)
+  })
 })
 
 server.listen(3000, () => {

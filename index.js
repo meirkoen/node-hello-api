1b6c12372da03c1afe2d37f82791fed9cbfacfbd
Meir Cohen <meirkoen@gmail.com>
2018-09-25 17:05:05 +0300

    run http server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello World!\n');
+});
+
+server.listen(3000, () => {
+  console.log('The server is listeninig on port 3000 now');
+});

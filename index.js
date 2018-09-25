861ec1bc43b5ba4dedbb37509afd39d1a0bd3af4
Meir Cohen <meirkoen@gmail.com>
2018-09-25 17:16:33 +0300

    parse the url and get trimmed path

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require('http');
+const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World!\n');
-});
+  const parsedUrl = url.parse(req.url, true)
+  const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+
+  res.end(`Request recived on path: ${trimmedPath}`)
+})
 
 server.listen(3000, () => {
-  console.log('The server is listeninig on port 3000 now');
-});
+  console.log('The server is listeninig on port 3000 now')
+})

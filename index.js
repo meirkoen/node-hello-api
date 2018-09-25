58f8b2bf0e4fcfbcb20e16d5ea1aa6afdbdad1f2
Meir Cohen <meirkoen@gmail.com>
2018-09-25 17:36:58 +0300

    respond with string of the path, method, query params and headers

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true)
   const trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
 
-  res.end(`Request recived on path: ${trimmedPath}`)
+  res.end(`
+    Request recived on path: ${trimmedPath}
+    with method: ${req.method.toUpperCase()}
+    and with these query string parameters: ${JSON.stringify(parsedUrl.query)}
+    and with these headers: ${JSON.stringify(req.headers)}
+    `)
 })
 
 server.listen(3000, () => {
